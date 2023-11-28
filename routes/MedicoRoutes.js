eb494cdb9f4e3eea7564c806e27ba820f9aaa70d
renanSC18 <109480132+renansc17@users.noreply.github.com>
2023-11-28 16:11:56 -0300

    Correção Médico Controller

--- routes/MedicoRoutes.js
+++ routes/MedicoRoutes.js
@@ -6,9 +6,9 @@ const MedicoController = require("../controllers/MedicoController");
 routesMedico.get("/medicos", auth, MedicoController.listar);
 routesMedico.get("/medicos/cadastro", auth, MedicoController.cadastrarRoute);
 routesMedico.post("/medicos/cadastro", MedicoController.cadastrar);
-routesMedico.get("/medicos/excluir/:id", UsuarioController.delete);
-routesMedico.get("/medicos/cadastro/:id",auth , auth, UsuarioController.update);
-routesMedico.post("/medicos/cadastro/:id", UsuarioController.updatePost);
+routesMedico.get("/medicos/excluir/:id", MedicoController.delete);
+routesMedico.get("/medicos/cadastro/:id",auth, MedicoController.update);
+routesMedico.post("/medicos/cadastro/:id", MedicoController.updatePost);
 routesMedico.get("/medicos/:id",auth , MedicoController.detalhar);
 
 module.exports = routesMedico;
